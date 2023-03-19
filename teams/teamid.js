e560a092deba394ce6631906b4fea172c6b5622b
TamannaRao <tamannarao2004@gmail.com>
2023-03-19 14:57:06 +0530

    Initial Commit

---
+++ teams/teamid.js
@@ -0,0 +1,8 @@
+var check=false;
+function uniqueid(){
+    if(check==false){
+        var d=new Date().getTime().toString();
+        alert("Your Team Code Will appear after clicking OK");
+        alert(d);
+    }
+}
